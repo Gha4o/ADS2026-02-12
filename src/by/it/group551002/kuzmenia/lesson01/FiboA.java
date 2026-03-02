0b98a19b14e1956e0522e5aee301966de153a979
gha4o <kuzmenazakhar999@gmail.com>
2026-03-02 23:50:42 +0300

    rearranged packages

--- src/by/it/group551002/kuzmenia/lesson01/FiboA.java
+++ src/by/it/group551002/kuzmenia/lesson01/FiboA.java
@@ -42,6 +42,7 @@ public class FiboA {
         //здесь нужно реализовать вариант без ограничения на размер числа,
         //в котором код совпадает с математическим определением чисел Фибоначчи
         //время O(2^n)
+
         if (n==0) return BigInteger.ZERO;
         if (n==1) return BigInteger.ONE;
         return slowA(n-1).add(slowA(n-2));
